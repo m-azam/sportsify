0b66e34ea151403a542811b377aca93c66551a70
Azam <mazam1488@gmail.com>
2021-10-23 23:59:22 -0400

    add detail activity

--- app/src/main/java/com/example/sportsify/DetailActivity.kt
+++ app/src/main/java/com/example/sportsify/DetailActivity.kt
@@ -2,12 +2,18 @@ package com.example.sportsify
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
+
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
 
+        val intent = intent // get Intent which we set from Previous Activity
+
+        selectedImage.setImageResource(intent.getIntExtra("image", 0))
     }
 
 }
